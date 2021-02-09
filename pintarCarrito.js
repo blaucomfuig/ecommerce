b5f8fbb8132ee6dbf451049174a07e2f3a6805f8
MAlexGG <alexandragalarzag@gmail.com>
2021-02-09 12:36:04 +0100

    vaciar Array Carrito

--- pintarCarrito.js
+++ pintarCarrito.js
@@ -82,8 +82,10 @@ function enviarPedido(){
     enviarMensaje();
     let totalPrecio = document.querySelector(".totalPrecio").innerHTML
     window.location.href ="https://api.whatsapp.com/send/?phone=" + numeroEmpresa + "&text="+ pedido + "Precio total =" + totalPrecio +"&app_absent=0";
+    carrito = [];
     retorno();
-    vaciarArray();
+
+
 }
     function enviarMensaje() {
         for (let i = 0; i < carrito.length; i++){
