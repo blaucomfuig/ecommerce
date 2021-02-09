491c8963462e1a8615582527b979a6bdf335d6a1
MAlexGG <alexandragalarzag@gmail.com>
2021-02-09 10:49:25 +0100

    reinicio

--- pintarCarrito.js
+++ pintarCarrito.js
@@ -79,11 +79,10 @@ function vaciarArray(){
 }
 
 function enviarPedido(){
-    location.reload();
     enviarMensaje();
     let totalPrecio = document.querySelector(".totalPrecio").innerHTML
     window.location.href ="https://api.whatsapp.com/send/?phone=" + numeroEmpresa + "&text="+ pedido + "Precio total =" + totalPrecio +"&app_absent=0";
-    
+    reinicio();
 }
     function enviarMensaje() {
         for (let i = 0; i < carrito.length; i++){
